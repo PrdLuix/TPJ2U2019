8ea99b1efd20815449babc612a6db2783f8dbaa1
Aluno <aluno@naverecife.local>
2019-10-31 08:44:50 -0300

    joint adicionado

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator anim;
+    PlayerBehaviour player;
+    //Controle de animação
+    void Start()
+    {
+        player = GetComponent<PlayerBehaviour>();
+        anim = GetComponent<Animator>();
+    }
+    public void Walk()
+    {
+        anim.SetBool("Walk", true);
+        anim.SetBool("Idle", false);
+    }
+    public void Idle()
+    {
+        anim.SetBool("Walk", false);
+        anim.SetBool("Idle", true);
+    }
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -18,32 +18,55 @@ public class PlayerBehaviour : MonoBehaviour
     float movimento;
     public VariableJoystick variableJoystick;
     public Canvas canvas;
-
-
+    public GameObject cameraCinematic;
+    PlayerAnimator player;
+    bool turnRight;
 
     void Start()
     {
+        turnRight = true;
         rb = GetComponent<Rigidbody2D>();
         tr = GetComponent<Transform>();
+        player = GetComponent<PlayerAnimator>();
     }
+    public float Movimento { get; set; }
     void Update()
     {
         estaNoChao = Physics2D.OverlapCircle(verificaChao.position, raioVchao, solido);
-
-        movimento = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(movimento * velocidade, rb.velocity.y);
         if (Input.GetButtonDown("Jump") && estaNoChao)
         {
             rb.AddForce(tr.up * forcaPulo);
 
             estaNoChao = false;
         }
-    }
+        if (movimento != 0 || variableJoystick.Horizontal != 0)
+        {
+            player.Walk();
+        }
+        else
+        {
+            player.Idle();
+        }
 
+        if ((movimento < 0 || variableJoystick.Horizontal < 0) && turnRight)
+        {
+            Flip();
+        }
+        else if ((movimento > 0 || variableJoystick.Horizontal > 0) && turnRight == false)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        turnRight = !turnRight;
+        tr.localScale = new Vector2(-tr.localScale.x, tr.localScale.y);
+    }
     public void FixedUpdate()
     {
-
-        rb.velocity = new Vector3(variableJoystick.Horizontal * velocidade, rb.velocity.y);
+        movimento = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movimento * velocidade, rb.velocity.y);
+        //rb.velocity = new Vector2(variableJoystick.Horizontal * velocidade, rb.velocity.y);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -54,6 +77,7 @@ public class PlayerBehaviour : MonoBehaviour
 
             DontDestroyOnLoad(this.gameObject);
             DontDestroyOnLoad(canvas);
+            DontDestroyOnLoad(cameraCinematic);
             transform.position = new Vector2(-6, -1);
         }
     }
